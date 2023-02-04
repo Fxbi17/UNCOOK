d9a958c5c18d7f6e9070594401b10a8d4e4ec001
Fabio Da Silva <fabio.dsv@hotmail.com>
2023-02-04 11:49:55 +0100

    done2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_04_103421) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_04_104933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_04_103421) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "shopping_lists", force: :cascade do |t|
+    t.bigint "ingredient_dish_id", null: false
+    t.bigint "menu_dish_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_dish_id"], name: "index_shopping_lists_on_ingredient_dish_id"
+    t.index ["menu_dish_id"], name: "index_shopping_lists_on_menu_dish_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -87,4 +96,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_04_103421) do
   add_foreign_key "menu_dishes", "dishes"
   add_foreign_key "menu_dishes", "menus"
   add_foreign_key "menus", "users"
+  add_foreign_key "shopping_lists", "ingredient_dishes"
+  add_foreign_key "shopping_lists", "menu_dishes"
 end
