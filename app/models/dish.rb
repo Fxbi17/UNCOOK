65fbe4f52e059694aba68311517675bb046136d7
bruno chehboun <bruno.chehboun@gmail.com>
2023-02-04 17:45:50 +0100

    validates-to-check

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,5 +1,9 @@
 class Dish < ApplicationRecord
   has_many :menu_dish
   has_many :ingredient_dish
-  has_many :review
+  has_many :reviews, dependent: :destroy
+
+  # validates :name, presence: true
+  # validates :description, presence: true
+  # validates :numpers, presence: true
 end
