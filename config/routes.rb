da02bce5908c9a4b3f5c3a243d0b2894883c1387
bruno chehboun <bruno.chehboun@gmail.com>
2023-02-14 22:01:33 +0100

    users_dashboard/favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :users, only: [:show]
   resources :dishes, only: [:index, :show] do
+    get "add_to_favorite", to: "dishes#add_to_favorite"
+    get "remove_from_favorite", to: "dishes#remove_from_favorite"
     resources :reviews, only: [:new, :create]
   end
 
